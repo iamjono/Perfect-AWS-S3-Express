1e9086923231717e61db661121a4e48ad7673fce
Jonathan Guthrie <jono@guthrie.net.nz>
2019-04-20 19:52:15 -0400

    update to work with swift 4+

--- Package.swift
+++ Package.swift
@@ -1,10 +1,18 @@
-// Generated automatically by Perfect Assistant Application
-// Date: 2017-10-23 15:27:24 +0000
+// swift-tools-version:4.0
+// Generated automatically by Perfect Assistant
+// Date: 2019-03-27 15:26:17 +0000
 import PackageDescription
+
 let package = Package(
 	name: "PerfectAWS",
-	targets: [],
+	products: [
+		.library(name: "PerfectAWS", targets: ["PerfectAWS"])
+	],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", "4.0.1"..<"5.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", "3.0.0"..<"4.0.0")
+	],
+	targets: [
+		.target(name: "PerfectAWS", dependencies: ["PerfectCURL", "PerfectCrypto"])
 	]
 )
